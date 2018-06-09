4757e9e3ac0d1ab28b30e94d3a327107b0736d2d
Raudy <maxbeltre@gmail.com>
2018-06-09 15:06:14 -0400

    My Django app, first commit

---
+++ django.py
@@ -0,0 +1,8 @@
+name = input("Cual es tu nombre?")
+last_name = input("Cual es tu apellido?")
+edad = int(input("Cual es tu edad?"))
+print("partido = ['1-PLD', '2- PRD', '3- UNV', '4- ROXB']")
+if  :
+    print ('')
+else:
+    print ('No puedes votar')
---
+++ funtion.py
@@ -0,0 +1,9 @@
+def hi(name):
+    print ("Hi " + name + '!')
+
+boys = ['Julio', 'Jorge', 'Raudy', 'victor']
+for name in boys:
+    hi(name)
+    print ('Next boy')
+
+# hi()
